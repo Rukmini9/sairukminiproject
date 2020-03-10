016e77fa7c1b94434e3160ccd67ef5cd17b9ed41
ravinderbargav <ravindergarigela29@gmail.com>
2020-03-10 11:13:11 +0530

    hello

---
+++ arrow.js
@@ -0,0 +1,6 @@
+var addNum = function (n1, n2) { return n1 + n2; };
+var multiNum = function (n1, n2) { return n1 * n2; };
+var dividNum = function (n1, n2) { return n1 / n2; };
+console.log(addNum(10, 20));
+console.log(multiNum(5, 6));
+console.log(dividNum(90, 3));
---
+++ arrow.ts
@@ -0,0 +1,6 @@
+let addNum=(n1:number,n2:number):number=>{return n1+n2;}
+let multiNum=(n1:number,n2:number):number=>{return n1*n2;}
+let dividNum=(n1:number,n2:number):number=>{return n1/n2;}
+console.log(addNum(10,20));
+console.log(multiNum(5,6));
+console.log(dividNum(90,3));
